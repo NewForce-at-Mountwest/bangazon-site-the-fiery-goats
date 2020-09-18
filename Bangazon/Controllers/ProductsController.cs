c57603df9a5dbb67c95066e65d97c1deb13d9d19
Derek Stapleton <derek.stapleton21@gmail.com>
2020-09-18 11:09:39 -0400

    user can view product details and add to cart

--- Bangazon/Controllers/ProductsController.cs
+++ Bangazon/Controllers/ProductsController.cs
@@ -35,7 +35,10 @@ namespace Bangazon.Controllers
             var product = await _context.Product
                 .Include(p => p.ProductType)
                 .Include(p => p.User)
+                .Include(p => p.OrderProducts)
                 .FirstOrDefaultAsync(m => m.ProductId == id);
+
+            product.Quantity -= product.OrderProducts.Count();
             if (product == null)
             {
                 return NotFound();
